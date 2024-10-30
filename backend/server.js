4503db9d8d5760b7cdbb1ae56be99ae2fb349e12
Heyson <heysonsilva98@gmail.com>
2024-10-30 02:32:26 -0300

    backend commit

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
-const dbConnection = require('./config/db.js')
+// import express from 'express';
 
+// const app = express()
+// const porta = 8080
+
+
+// app.get("/", (req, res)=> {
+//     res.send("Rota Raiz Está Funcionando !!")
+// })
+
+// app.listen(porta, () => {
+//     console.log("is running...")
+// })
